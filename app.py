21a89bbbdbb7774751a0dc76b1fc98b0c70cbf23
dardg24 <danielrdegouveia@gmail.com>
2023-10-18 16:11:20 +0100

    branch ticket 3 - epic branch 1

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+'''
+Cargamos los datos
+'''
+train_df = pd.read_csv('data/train.csv')
+test_df = pd.read_csv('data/test.csv')
