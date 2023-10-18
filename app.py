297c54d7331b08f0b18e03c92c539165cbcaf3c9
julyvargas92 <julyetsiv@gmail.com>
2023-10-18 16:12:39 +0200

    july Ticket 1 - epicbranch 3

---
+++ app.py
@@ -0,0 +1,7 @@
+
+'''
+Generamos Variables dependientes e independientes
+'''
+variables=['Pclass', 'Age', 'Sex', 'SibSp', 'Parch', 'Fare', 'CabinBool', 'Embarked_C', 'Embarked_S', 'Embarked_Q']
+X = train_df[variables]
+y = train_df['Survived']
